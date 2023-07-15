400cca4dd0d45e735213a732979ebec8065a4882
kelvin <denlinato@gmail.com>
2023-07-15 16:45:34 +0000

    appending and adding to a slcie

--- lesson4/slice.go
+++ lesson4/slice.go
@@ -35,5 +35,23 @@ func main(){
 	  nums1 :=  numbers ;
 	  nums1[1] =  3 
 	  fmt.Println(numbers) 
+       
 
+	  //append to a slice or copy to a slice
+    
+	 numbers1 := []int{1,2,3,4} 
+
+     	numbers=  append(numbers1 , 10 )
+    fmt.Println(numbers) ;
+	n3 := []int{100 , 200} 
+	numbers1 =  append(numbers1, n3...) 
+	
+	   src:= []int{10,20,30} 
+	   dst:=  make([]int,  len(src)) ;
+	       copy(dst , src) ;
+	   
+	   fmt.Println(src , dst ) ;
+	
+
+	
 }
